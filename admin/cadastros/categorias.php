63fa44f060dfbf464509a3e4c4dd9153896ef13c
Daniel Oliveira <danielmo352@gmail.com>
2024-04-09 20:19:04 -0300

    form

--- admin/cadastros/categorias.php
+++ admin/cadastros/categorias.php
@@ -1,7 +1,7 @@
 <div class="card">
     <div class="card-header">
         <h2>Cadastro de categorias</h2>
-        
+
         <div class="float-right">
             <a href="listar/categorias" class="btn btn-success">
                 Listar Categorias
@@ -9,6 +9,23 @@
         </div>
     </div>
     <div class="card-body">
+        <form action="salvar/categorias" method="POST"> </form>
+        <label for="nome">Nome da Categoria:</label>
+        <input 
+        type="text"
+        name="nome"
+        id="nome"
+        class="form-control"
+        required
+        >
+
+        <br>
+
+        <button 
+        type="submit" class="btn btn-success">Salvar Dados 
+        </button>
+
+
 
     </div>
 </div>
